44134e63afdd9d51a2c667eba6f55c0926fb8f76
prudhvirajkrishna <prudhviraj123k@gmail.com>
2022-11-08 12:13:40 +0530

    weather commit

---
+++ App3.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from './logo.svg';
+import "./App.css";
+import Weather from "./weather";
+
+function App3() {
+  return (
+    <div className="App">
+      <Weather />
+      
+    </div>
+  );
+}
+
+export default App3
